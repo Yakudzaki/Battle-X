6bb54871980beade4aad0262e87d348e0fe21659
Yakudzaki <123520462+Yakudzaki@users.noreply.github.com>
2023-04-22 17:11:33 +0600

    Update help.py

--- Bot/handlers/user/help.py
+++ Bot/handlers/user/help.py
@@ -1,13 +1,13 @@
 from loader import dp, bot
 from aiogram import types
-from aiogram.dispatcher.filters import CommandStart
+from aiogram.dispatcher.filters import Command
 from loguru import logger
 from utils.db.db_utils import create_user, user_exists
 
 
 # Сработает если юзера нет в бд
 @dp.message_handler(lambda m: user_exists(m.from_user.id) is False)
-async def start(message: types.Message):
+async def help(message: types.Message):
     user_id = message.from_user.id
     create_user(user_id)
     await message.answer('⚙️ Вот список команд, которые вы можете использовать:'
@@ -27,8 +27,8 @@ async def start(message: types.Message):
     '❓ Если у вас возникли вопросы, воспользуйтесь командой /report (Сообщение), чтобы связаться с администраторами бота.')
 
 
-@dp.message_handler(CommandStart())
-async def start(message: types.Message):
+@dp.message_handler(Command("help"))
+async def help(message: types.Message):
     await message.answer('⚙️ Вот список команд, которые вы можете использовать:'
     
     '/start - Запустить бота.'
