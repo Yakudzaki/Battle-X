f60a64147c1ed08ca423cd2de2401156ddb0feb4
Yakudzaki <123520462+Yakudzaki@users.noreply.github.com>
2023-05-07 02:35:13 +0600

    Update help_kb.py

--- Bot/keyboards/inline/help_kb.py
+++ Bot/keyboards/inline/help_kb.py
@@ -1,1 +1,11 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+help_adm = InlineKeyboardMarkup(row_width=1,
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Админ 1', url='https://t.me/Yakudza_Drill'),
+            InlineKeyboardButton(text='Админ 2', url='https://t.me/x_FouR_x')
+        ],
+    ]
+)
