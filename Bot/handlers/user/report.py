58fe7394fcd1cd36b6b1f1fb36e115db0016fc0f
Yakudzaki <123520462+Yakudzaki@users.noreply.github.com>
2023-05-09 03:29:07 +0600

    Update report.py
    Изменил текст репорта как и хотела по мелочи.

--- Bot/handlers/user/report.py
+++ Bot/handlers/user/report.py
@@ -14,11 +14,11 @@ async def report_handler(message: types.Message):
         db_user = get_user(tg_user.id)
         
         for admin in ADMINS:
-            await bot.send_message(admin, f"Пришел репорт от <a href='tg://user?id={tg_user.id}'>{tg_user.first_name}</a>!\n"
+            await bot.send_message(admin, f"⚠️ Пришел репорт от <a href='tg://user?id={tg_user.id}'>{tg_user.first_name}</a>!\n\n"
                              f'<b>BU ID</b> - <code>{db_user.bu_id}</code>\n'
                              f'<b>TG ID</b> - <code>{tg_user.id}</code>\n'
                              f'<b>MESSAGE</b> - {"".join(x + " " for x in cause)}\n')
-        await message.answer('Ваше сообщение было отправлено администраторам!')
+        await message.answer('✅ Ваше сообщение было отправлено администраторам, ожидайте ответа!')
     else:
-        await message.answer('<b>Введите сообщение репорта!</b>')
+        await message.answer('<b>⚠️ Введите сообщение репорта!</b>')
 
