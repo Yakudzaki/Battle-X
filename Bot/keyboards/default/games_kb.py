58786d1d6ba27e4e33e89bbf56c384ee116823e8
Yakudzaki <123520462+Yakudzaki@users.noreply.github.com>
2023-05-08 14:45:21 +0600

    Update games_kb.py

--- Bot/keyboards/default/games_kb.py
+++ Bot/keyboards/default/games_kb.py
@@ -4,7 +4,12 @@ games = ReplyKeyboardMarkup(row_width=2,
     keyboard=[
         [
             KeyboardButton(text='Минёр 💣'),
-            KeyboardButton(text='Трейд 📊')
+            KeyboardButton(text='Трейд 📊'),
+            KeyboardButton(text='Монетка 💱'),
+            KeyboardButton(text='Классик 🎫'),
+            KeyboardButton(text='Dice ↕️'),
+            KeyboardButton(text='Кости 🎲')
+            KeyboardButton(text='Дэуль 🔫')
         ],
     ], input_field_placeholder='Выберите игру', resize_keyboard=True, one_time_keyboard=True
 )
