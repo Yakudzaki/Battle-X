395620b7c069c174a5279231c624c0df1578f630
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-05-12 00:48:02 +0600

    Update topup_kb.py
    добавил демо кнопки для выбора способа пополнения.

--- Bot/keyboards/inline/topup_kb.py
+++ Bot/keyboards/inline/topup_kb.py
@@ -1,1 +1,18 @@
+from aiogram.types import
+InlineKeyboardMarkup, InlineKeyboardButton 
 
+
+topup_kb = InlineKeyboardMarkup(row_width=1, 
+     inline_keyboard=[ 
+         [ 
+             InlineKeyboardButton(text='🥝 Qiwi', callback_data='topup_qiwi')
+         ],
+
+         [
+             InlineKeyboardButton(text='🅿️ Payeer', callback_data='topup_payeer')
+         ],
+         [
+             InlineKeyboardButton(text='💎 TON', callback_data='topup_ton') 
+         ]
+     ] 
+ )
