8ac0b8027e0cfbbccda894af1e4b931d65afb370
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-05-12 00:10:18 +0600

    Update statist.py
    добавил кнопочку назад

--- Bot/handlers/admin/callback/statist.py
+++ Bot/handlers/admin/callback/statist.py
@@ -2,6 +2,7 @@ from loader import dp, bot
 from aiogram import types
 from filters.admin_only import IsAdminCb
 from utils.db.db_utils import *
+from keyboards.inline.admin_kb import back_btn
 
 @dp.callback_query_handler(lambda call: call.data.endswith('stat'), IsAdminCb())
 async def admin_statistick(call: types.CallbackQuery):
@@ -10,5 +11,5 @@ async def admin_statistick(call: types.CallbackQuery):
     await call.message.edit_text(
         '📊 <b>Статистика бота</b>\n\n'
         f'🔢 Всего юзеров в боте - <code>{users_in_bot}</code>\n'
-        f'💸 Общий баланс у все юзеров - <code>{balance_all_users}</code>'
+        f'💸 Общий баланс у все юзеров - <code>{balance_all_users}</code>', reply_markup=back_btn
     )
