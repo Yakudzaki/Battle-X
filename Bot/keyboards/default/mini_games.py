508d4a343ba6107f69b1a424d71e039c76c38e00
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-05-16 23:07:53 +0600

    Create mini_games.py

---
+++ Bot/keyboards/default/mini_games.py
@@ -0,0 +1,35 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+mini_games = ReplyKeyboardMarkup(row_width=2,
+
+    keyboard=[
+
+
+
+        [
+
+            KeyboardButton(text='Баскетбол 🏀'),
+
+            KeyboardButton(text='Футбол ⚽')
+
+        ],
+
+        [
+
+            KeyboardButton(text='Дартс 🎯'),
+
+            KeyboardButton(text='Боулинг 🎳')
+
+        ],
+
+        [
+
+            KeyboardButton(text='Слоты 🎰'),
+
+            KeyboardButton(text='Кости 🎲')
+
+        ],
+
+    ], input_field_placeholder='Выберите игру', resize_keyboard=True, one_time_keyboard=True
+
+)
