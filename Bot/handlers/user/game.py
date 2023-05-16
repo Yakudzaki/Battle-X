04f86532c634b4f0c95a3365da1f46b22c875eac
Yakudza <123520462+Yakudzaki@users.noreply.github.com>
2023-05-16 23:19:37 +0600

    Update game.py

--- Bot/handlers/user/game.py
+++ Bot/handlers/user/game.py
@@ -2,6 +2,7 @@ from loader import dp
 from aiogram import types
 from aiogram.dispatcher.filters import Command
 from keyboards.default.games_kb import games
+from keyboards.default.mini_games_kb import mini_games
 
 from utils.db.db_utils import get_user
 
@@ -16,3 +17,12 @@ async def game(message: types.Message):
 async def game(message: types.Message):
     nickname = get_user(message.from_user.id).nickname
     await message.answer(f'{nickname}, выберите одну из игр', reply_markup=games)
+
+@dp.message_handler(text='Мини-Игры 👾')
+
+async def mini_game(message: types.Message):
+
+    nickname = get_user(message.from_user.id).nickname
+
+    await message.answer(f'{nickname}, выберите одну из игр', reply_markup=mini_games)
+    
